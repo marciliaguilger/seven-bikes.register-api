1b11fba6dc2aba496c35aa2096ea1b12aa389765
marcilia.guilger@ifood.com.br <marcilia.guilger@ifood.com.br>
2025-05-18 17:33:20 -0300

    create project

---
+++ register-api/src/main/kotlin/br/com/seven_bikes/register_api/register_api/RegisterApiApplication.kt
@@ -0,0 +1,11 @@
+package br.com.seven_bikes.register_api.register_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RegisterApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<RegisterApiApplication>(*args)
+}
---
+++ register-api/src/test/kotlin/br/com/seven_bikes/register_api/register_api/RegisterApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.seven_bikes.register_api.register_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RegisterApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
